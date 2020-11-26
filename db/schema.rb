dc30962af394923423694eb7afd63401b8723127
yuki33333333 <piya.yuki.0828@gmail.com>
2020-11-27 00:33:44 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_152429) do
+ActiveRecord::Schema.define(version: 2020_11_26_152856) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
