063353787d2abfbfc0df39ce1fd6fb30dbe77cc9
yuki33333333 <piya.yuki.0828@gmail.com>
2021-01-04 15:03:03 +0900

    jwtの初期設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  # 安全なエンドポイントを守るための認証アクションを提供するモジュール
+  include JWTSessions::RailsAuthorization
+  # 例外処理
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  private
+
+  def not_authorized
+    render json: { error: 'Not authorized' }, status: :unauthorized
+  end
 end
