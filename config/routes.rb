0ba4c06c82ac9e28d124d2f59d60efbf67bbf520
yuki33333333 <piya.yuki.0828@gmail.com>
2021-01-04 17:18:01 +0900

    jwt認証

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
+
+  resources :todos
 end
